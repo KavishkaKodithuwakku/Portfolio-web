205e78a499ff3c7b77f198b5f43162c24e9a0a29
KavishkaKodithuwakku <kavishkakodithuwakku21@gmail.com>
2026-01-17 14:02:45 +0530

    create the projects option

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -12,6 +12,7 @@ function App() {
       <NavBar />
       <Banner />
       <Skills />
+      <Projects />
     </div>
   );
 }
